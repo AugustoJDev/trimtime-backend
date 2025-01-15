78b0a5e0cd4227b04637ca39b3bcde18b4d76750
jonathan@JK-Notebook <augustojdev@gmail.com>
2025-01-15 09:28:07 -0300

    Added firebaseConfig file

---
+++ src/secrets/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyZpEA1oX4Z-iv18siUIhxU_K9ym-BFvs",
+  authDomain: "trimtime-jk.firebaseapp.com",
+  projectId: "trimtime-jk",
+  storageBucket: "trimtime-jk.firebasestorage.app",
+  messagingSenderId: "979218248379",
+  appId: "1:979218248379:web:f9f72858bd5ee6dfbe2f9e"
+};
+
+exports.firebaseConfig = firebaseConfig;
