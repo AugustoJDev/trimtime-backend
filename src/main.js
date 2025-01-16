1c4e6557e23ccc31a1fbd0e995a61bf61e86cdc6
jonathan@JK-Notebook <augustojdev@gmail.com>
2025-01-16 10:15:43 -0300

    Enabled cors to frontend

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'https://trimtime-frontend.pages.dev',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 const endpoints = require('./routes');
 app.use(endpoints);
