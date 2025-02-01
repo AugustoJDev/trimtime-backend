45cb0d36628c83197b39962fda248c44aab68c2d
Jonathan <augustojdev@gmail.com>
2025-02-01 19:01:35 -0300

    Update firebaseConfig.js

--- src/secrets/firebaseConfig.js
+++ src/secrets/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAyZpEA1oX4Z-iv18siUIhxU_K9ym-BFvs",
-  authDomain: "trimtime-jk.firebaseapp.com",
-  projectId: "trimtime-jk",
-  storageBucket: "trimtime-jk.firebasestorage.app",
-  messagingSenderId: "979218248379",
-  appId: "1:979218248379:web:f9f72858bd5ee6dfbe2f9e"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 exports.firebaseConfig = firebaseConfig;
